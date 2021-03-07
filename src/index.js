665d0041d66e77dbf2ae289b3f81ba784d354acc
sondluu <son.d.luu@gmail.com>
2021-03-07 17:56:42 -0500

    another way to handle authorization
    another way to handle authorization but making shorter code

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.render(
 
   <React.StrictMode>
     {/* <React.Fragment> */}
-    <App authorized={true}/>,
+    <App authorized={false}/>,
     {/* <AppTwo /> */}
     {/* </React.Fragment> */}
   </React.StrictMode>,
