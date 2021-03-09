09c7ad133e25fd1bf9972c86b3e70588647e90b8
sondluu <son.d.luu@gmail.com>
2021-03-09 00:28:23 -0500

    Handling API data loading state
    An example of how the process of loading data can be broken down to separate states, for example, loading > complete > error ... and how data and render can be handled in each state

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ import React, { useState, useEffect } from "react";
 
 function App({ login }) { 
   const [data, setData] = useState(null); //sending initial state of null due to no data upon app loading
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
 
   //next useEffect takes in a callback function
   useEffect(() => {
+    if(!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)  //re-structuring the link to include a way to log in 
       .then((response) => response.json())
-      .then(setData);
-  }, []); //passing in an empty array b/c we ony want to fetch data when the component renders
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]); //passing in an empty array b/c we ony want to fetch data when the component renders
 
-  if (data) {
+  if (loading) return <h1>Loading...</h1>;
+  if (error)
+    return <prep>{JSON.stringify(error, null, 2)}</prep>; //the number 2 here is to format the json file so we can read it
+  if (!data) return null;
     return (
     <div>
       <h1>{data.name}</h1>
@@ -23,11 +32,6 @@ function App({ login }) {
       <img alt={data.login} src={data.avatar_url} />
     </div> //stringify the data.
     );
-  }
-
-  return ( //the div below is for parsing data coming back from api
-    <div>No user data available </div>
-  );
 }
 
 export default App;
