3bd1c73a27645dbcc8d9ed95cfd9b07a67e58b4a
sondluu <son.d.luu@gmail.com>
2021-03-08 23:15:25 -0500

    useState function continued
    another way to check if the checkbox is checked or unchecked , by creating a separate function toggle(), and just pass this function toggle() after "onchange" is executed.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 
 
 
 function App() { 
   const [checked, setChecked] = useState(false);
+
+
+  function toggle() {
+    setChecked(checked => !checked);
+  }
+
+
+
   return (
     <>
-      <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)}
+      <input  type="checkbox" 
+              value={checked} 
+              onChange={(toggle)}
       />
       <p>{checked ? "checked" : "not checked"}</p>
     </>
