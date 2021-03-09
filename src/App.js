7309637cae19594c813a87a9e1cd05212c09dc0f
sondluu <son.d.luu@gmail.com>
2021-03-08 23:09:58 -0500

    useState function
    useState, checking whether checkbox is checked and then render confirmation that box is checked or box is unchecked.

--- src/App.js
+++ src/App.js
@@ -5,27 +5,13 @@ import React, { useState, useEffect } from "react";
 
 
 
-function App() { //de-structuring object to give it a name "engagement"
-  const [engagement, setEngagement] = useState("High"); //useState function returns an array of 2 items: 1st item is state, 2nd item is a function that updates the state.
-  //above, engagement is the 1st item which is the state, and the 2nd "setEngagement" is a function to change state
-  
-  const [surveycompletion, setSurveyComplete] = useState("Completed");
-  
-  useEffect(() => {
-    console.log(`Engagement is ${engagement}`); //could be use as a side effect here, doesn't have to be for rendering
-  }, [engagement]);
-
-  useEffect(() => {
-    console.log(`survey ${surveycompletion}`);
-  }, [surveycompletion]);
-
+function App() { 
+  const [checked, setChecked] = useState(false);
   return (
     <>
-    <h1>Engagement Level is: {engagement} and {surveycompletion}</h1>
-    <button onClick={() => setEngagement("Low")}>Low</button>
-    <button onClick={() => setEngagement("High")}>High</button>
-    <button onClick={() => setSurveyComplete("Completed")}>Completed</button>
-    <button onClick={() => setSurveyComplete("Uncompleted")}>Uncompleted</button>
+      <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)}
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
     </>
   );
 }
