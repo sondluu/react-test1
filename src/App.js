56abb9f6f4fcd87ff902e6dcdec4628a5235232b
sondluu <son.d.luu@gmail.com>
2021-03-08 23:28:36 -0500

    useReducer method
    the actual useReducer method is imported, and used

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 
 
 
 function App() { 
-  const [checked, setChecked] = useState(false);
+  const [checked, toggle] = useReducer( //pass the function toggle which is the update function inside the [] 
+    (checked) => !checked, //useReducer takes in 2 arguments, the first if the reducer function
+    false //second arguent is the original state
+  );
 
-
-  function toggle() {
-    setChecked(checked => !checked); //this can be called a "reducer"
-  }
 
 //a reducer function's definition is it takes in the current state ad returns
 //a new state. In this partcular case, if "checked" is false then returns the opposit which is true
- 
+
 
   return (
     <>
       <input  type="checkbox" 
               value={checked} 
-              onChange={(toggle)}
+              onChange={toggle}
       />
       <p>{checked ? "checked" : "not checked"}</p>
     </>
